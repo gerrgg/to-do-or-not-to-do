c9aa60a696f6638a521e7f1e881254117c4c6f47
Gregory Bastianelli <greg@gerrg.com>
2020-07-23 16:29:14 -0400

    switch tabs and hide the sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-const todoOrNotTodo = (() => {
-    let tabLinks = document.getElementsByClassName('tab-link');
+const DOM = (() => {
 
-    for( let tabLink of tabLinks ){
-        tabLink.addEventListener( 'click', getTabContent() )
+    const switchTab = ( e ) => {
+        /**
+         * On click, set the clicked items class to .active and show the element in data-target
+         */
+        let elementToShow = document.getElementById(e.target.attributes["data-target"].nodeValue);
+
+        // remove class .active from tab links and .show from inner workings
+        for( let tabLink of tabLinks ){
+            let elementToHide = document.getElementById(tabLink.attributes["data-target"].nodeValue);
+            tabLink.classList.remove('active');
+            elementToHide.classList.remove('show');
+        }
+
+        // add .active class to target
+        e.target.classList.add('active');
+
+        // show element specified in target's data-target attribute
+        elementToShow.classList.add('show');
     }
+
+    const slideMenu = () => {
+        /**
+         * On click, show and hide the sidebar
+         */
+        let visibility = ( sidebar.style.visibility === 'visible' ) ? 'collapse' : 'visible'
+        sidebar.style.visibility = visibility;
+    }
+    
+    // elements
+    let tabLinks = document.getElementsByClassName('tab-link');
+    let menuButton = document.getElementById('menu-button');
+    let sidebar = document.getElementById('project-sidebar')
+
+    // listenersYes 
+    for( let tabLink of tabLinks ){
+        tabLink.addEventListener( 'click', switchTab )
+    }
+
+    menuButton.addEventListener( 'click', slideMenu )
 
 
 })();
